ef4cb01250813430282b4cf43b5a2a09d8b73a41
iamamohamad <mmdmohamad86@gmail.com>
2023-05-29 15:53:45 +0430

    new cam

--- offline.js
+++ offline.js
@@ -1,26 +1,19 @@
 ﻿{
-	"version": 1682963467,
+	"version": 1685359392,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
+		"images/snapshotsprite-sheet0.png",
+		"images/tiledbackground.png",
 		"images/sprite-sheet0.png",
-		"images/humanbody-sheet0.png",
-		"images/sprite2-sheet0.png",
-		"images/sprite3-sheet0.png",
-		"images/chlorine1_einpixio-sheet0.png",
-		"images/fzch32_inpixio-sheet0.png",
-		"images/sprite4-sheet0.png",
-		"images/sprite5-sheet0.png",
-		"images/sprite5-sheet1.png",
-		"images/sprite6-sheet0.png",
-		"mediayes/0bikalam download1music.ir .ogg",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
-		"loading-logo.png"
+		"loading-logo.png",
+		"adapter.js"
 	]
 }
